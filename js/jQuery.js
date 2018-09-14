27a7d51e3b985752a6934a8249ed2da9c493ea89
Lucas Vieira <tiomedianope@gmail.com>
2018-09-14 18:58:07 -0300

    Cores

--- js/jQuery.js
+++ js/jQuery.js
@@ -8,11 +8,36 @@ $(function(){
 
   $(this).on('keydown', function(key){
     if (key.keyCode == '65') {
-      $('p#keyBoard-1').removeClass().addClass('roxo');
+      $('p#keyBoard-1').removeClass().addClass('roxo').css('background', '#e4aaff');
+      setTimeout(function(){
+        $('p#keyBoard-1').css('background', 'transparent');
+        $('p#keyBoard-2').removeClass().addClass('roxo').css('background', '#e4aaff');
+      }, 230);
+      setTimeout(function(){
+        $('p#keyBoard-2').css({'background': 'transparent', 'border': '6px solid transparent', 'boxShadow': 'none', 'transition': '1.2s'});
+        $('p#keyBoard-3').removeClass().addClass('roxo').css('background', '#e4aaff');
+      }, 720);
+      setTimeout(function(){
+        $('p#keyBoard-3').css({'background': 'transparent', 'transition': '1.2s'});
+        $('p#keyBoard-4').removeClass().addClass('roxo').css('background', '#e4aaff');
+      }, 900);
       $('#audio-1')[0].load();
       $('#audio-1')[0].play();
-    } else if (key.keyCode == '83') {
-      $('p#keyBoard-2').removeClass().addClass('verde');
+    }
+    else if (key.keyCode == '83') {
+      $('p#keyBoard-2').removeClass().addClass('amarelo');
+      setTimeout(function(){
+        $('p#keyBoard-8').removeClass().addClass('amarelo');
+      }, 100);
+      setTimeout(function(){
+        $('p#keyBoard-7').removeClass().addClass('amarelo');
+      }, 800);
+      setTimeout(function(){
+        $('p#keyBoard-6').removeClass().addClass('amarelo');
+      }, 1700);
+      setTimeout(function(){
+        $('p#keyBoard-5').removeClass().addClass('amarelo');
+      }, 2600);
       $('#audio-2')[0].load();
       $('#audio-2')[0].play();
     } else if (key.keyCode == '68') {
@@ -20,15 +45,15 @@ $(function(){
       $('#audio-3')[0].load();
       $('#audio-3')[0].play();
     } else if (key.keyCode == '70') {
-      $('p#keyBoard-4').removeClass().addClass('verde');
+      $('p#keyBoard-4').removeClass().addClass('azul');
       $('#audio-4')[0].load();
       $('#audio-4')[0].play();
     } else if (key.keyCode == '71') {
-      $('p#keyBoard-5').removeClass().addClass('verde');
+      $('p#keyBoard-5').removeClass().addClass('vermelho');
       $('#audio-5')[0].load();
       $('#audio-5')[0].play();
     } else if (key.keyCode == '72') {
-      $('p#keyBoard-6').removeClass().addClass('verde');
+      $('p#keyBoard-6').removeClass().addClass('rosa');
       $('#audio-6')[0].load();
       $('#audio-6')[0].play();
     } else if (key.keyCode == '90') {
@@ -36,13 +61,53 @@ $(function(){
       $('#audio-7')[0].load();
       $('#audio-7')[0].play();
     } else if (key.keyCode == '88') {
-      $('p#keyBoard-8').removeClass().addClass('verde');
+      $('p#keyBoard-8').removeClass().addClass('vermelho');
+      setTimeout(function(){
+        $('p#keyBoard-1').removeClass().addClass('vermelho');
+      }, 750);
+      setTimeout(function(){
+        $('p#keyBoard-2').removeClass().addClass('vermelho');
+      }, 950);
+      setTimeout(function(){
+        $('p#keyBoard-3').removeClass().addClass('vermelho');
+      }, 1050);
+
+      setTimeout(function(){
+        $('p#keyBoard-3').removeClass().addClass('amarelo');
+      }, 1600);
+      setTimeout(function(){
+        $('p#keyBoard-2').removeClass().addClass('amarelo');
+      }, 1700);
+      setTimeout(function(){
+        $('p#keyBoard-1').removeClass().addClass('amarelo');
+      }, 1800);
+
+      setTimeout(function(){
+        $('p#keyBoard-1').removeClass().addClass('verde');
+      }, 2400);
+      setTimeout(function(){
+        $('p#keyBoard-2').removeClass().addClass('verde');
+      }, 2500);
+      setTimeout(function(){
+        $('p#keyBoard-3').removeClass().addClass('verde');
+      }, 2600);
+
+      setTimeout(function(){
+        $('p#keyBoard-3').removeClass().addClass('azul');
+      }, 3400);
+      setTimeout(function(){
+        $('p#keyBoard-2').removeClass().addClass('azul');
+      }, 3500);
+      setTimeout(function(){
+        $('p#keyBoard-1').removeClass().addClass('azul');
+      }, 3600);
+
       $('#audio-8')[0].load();
       $('#audio-8')[0].play();
     };
   });
 
-  $(this).on('keyup', function(key){
+  $(this).on('keyup', function(){
     $('p').addClass('hover');
   });
 
